455e68a865c491e51e6c6c3bd7015c704c75ad1a
fuyingtao <meishadevs@gmail.com>
2022-09-19 13:51:48 +0800

    字段采用驼峰命名法命名

--- model/user.js
+++ model/user.js
@@ -3,14 +3,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  user_name: String,
+  // 用户名
+  userName: String,
+
+  // 密码
   password: String,
-  id: Number,
-  create_time: String,
-  user: { type: String, default: '管理员' },
-  status: Number, // 1:普通管理、 2:超级管理员
-  avatar: { type: String, default: 'default.jpg' },
-  city: String
+
+  // 邮箱
+  email: String,
+
+  // 创建时间
+  createTime: String,
+  id: Number
 });
 
 userSchema.index({ id: 1 });
