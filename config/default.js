799c6cdcfb4c79290cc576bdbf2d4706e9272743
meishadevs <meishadevs@gmail.com>
2022-10-17 14:53:42 +0800

    修改连接的数据库

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
   port: 8002,
-  url: 'mongodb://localhost:27017/dswz',
+  url: 'mongodb://localhost:27017/store',
   session: {
-    name: 'SID',
-    secret: 'SID',
+    name: 'store',
+    secret: 'store',
     cookie: {
       httpOnly: true,
       secure: false,
