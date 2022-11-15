79518406fb645bb9d9825e7ad5c9c81eb866bc68
meishadevs <meishadevs@gmail.com>
2022-11-15 16:27:26 +0800

    添加获取权限数据的api接口

--- controller/menu.js
+++ controller/menu.js
@@ -5,6 +5,7 @@ class Menu extends BaseComponent {
   constructor() {
     super();
     this.getList = this.getList.bind(this);
+    this.getPermissionList = this.getPermissionList.bind(this);
   }
 
   // 获得菜单列表
@@ -25,6 +26,27 @@ class Menu extends BaseComponent {
       res.send(this.successMessage(null, data));
     } catch (err) {
       res.send(this.failMessage('获取菜单列表失败'));
+    }
+  }
+
+  // 获得权限列表
+  async getPermissionList(req, res, next) {
+    try {
+      // 获得菜单列表
+      // -_id 表示不显示 _id 字段
+      // { sort: 1 } 表示根据 sort 值升序排列
+      const permissionList = await MenuModel.find({}, '-_id -permission -url -icon -sort -type -remark').sort({ sort: 1 }).lean();
+
+      // 将菜单列表转成一颗树的结构
+      const treeData = this.arrayToTree(permissionList);
+
+      let data = {
+        list: treeData
+      };
+
+      res.send(this.successMessage(null, data));
+    } catch (err) {
+      res.send(this.failMessage('获取权限数据失败'));
     }
   }
 }
--- routes/menuApi.js
+++ routes/menuApi.js
@@ -5,6 +5,11 @@ const menuApi = (router) => {
   router.get('/menu/list', (req, res, next) => {
     Menu.getList(req, res, next);
   });
+
+   // 获得权限列表
+   router.get('/menu/permissions_list', (req, res, next) => {
+    Menu.getPermissionList(req, res, next);
+  });
 };
 
 module.exports = menuApi;
