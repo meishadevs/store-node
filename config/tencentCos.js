afdfc5dd6aeadf001e5d6b29d29005235e10e74f
meishadevs <meishadevs@gmail.com>
2022-11-30 10:40:45 +0800

    隐藏腾讯云访问秘钥

--- config/tencentCos.js
+++ config/tencentCos.js
@@ -1,6 +1,6 @@
 module.exports = {
-  secretId: 'AKIDd8TdaIm258wXw75oKcfS2Op0ymNkvxnO',
-  secretKey: 'OZYIn492kFZgbN1UkofJk05zhGcmiRyJ',
+  secretId: 'A**********************************O',
+  secretKey: 'O**********************************J',
   bucket: 'store-1253560230',
   region: 'ap-guangzhou'
 };
